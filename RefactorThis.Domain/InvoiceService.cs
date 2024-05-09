a4f4c5546bb4a73c6c9e5d9c08690b7b9e7e6703
badicloud2011 <badicloud2011@gmail.com>
2024-05-10 01:23:20 +0800

    - Break down the method into smaller, more manageable functions. Each function performs a single task.

--- RefactorThis.Domain/InvoiceService.cs
+++ RefactorThis.Domain/InvoiceService.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RefactorThis.Domain.Interface;
 using RefactorThis.Persistence;
+using RefactorThis.Persistence.Interface;
 using Microsoft.Extensions.Logging; // Used this package for logging purposes and to improve debuggability.
+using RefactorThis.Persistence.Enums;
 
 namespace RefactorThis.Domain
 {
@@ -17,139 +22,54 @@ namespace RefactorThis.Domain
             _logger = logger;
         }
 
-		public string ProcessPayment( Payment payment )
-		{
-			var inv = _invoiceRepository.GetInvoice( payment.Reference );
+        public async Task<string> ProcessPayment(Payment payment, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var invoice = await GetInvoice(payment.Reference, cancellationToken);
 
-			var responseMessage = string.Empty;
+                var responseMessage = string.Empty;
 
-			if ( inv == null )
-			{
-				throw new InvalidOperationException( "There is no invoice matching this payment" );
-			}
-			else
-			{
-				if ( inv.Amount == 0 )
-				{
-					if ( inv.Payments == null || !inv.Payments.Any( ) )
-					{
-						responseMessage = "no payment needed";
-					}
-					else
-					{
-						throw new InvalidOperationException( "The invoice is in an invalid state, it has an amount of 0 and it has payments." );
-					}
-				}
-				else
-				{
-					if ( inv.Payments != null && inv.Payments.Any( ) )
-					{
-						if ( inv.Payments.Sum( x => x.Amount ) != 0 && inv.Amount == inv.Payments.Sum( x => x.Amount ) )
-						{
-							responseMessage = "invoice was already fully paid";
-						}
-						else if ( inv.Payments.Sum( x => x.Amount ) != 0 && payment.Amount > ( inv.Amount - inv.AmountPaid ) )
-						{
-							responseMessage = "the payment is greater than the partial amount remaining";
-						}
-						else
-						{
-							if ( ( inv.Amount - inv.AmountPaid ) == payment.Amount )
-							{
-								switch ( inv.Type )
-								{
-									case InvoiceType.Standard:
-										inv.AmountPaid += payment.Amount;
-										inv.Payments.Add( payment );
-										responseMessage = "final partial payment received, invoice is now fully paid";
-										break;
-									case InvoiceType.Commercial:
-										inv.AmountPaid += payment.Amount;
-										inv.TaxAmount += payment.Amount * 0.14m;
-										inv.Payments.Add( payment );
-										responseMessage = "final partial payment received, invoice is now fully paid";
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-								
-							}
-							else
-							{
-								switch ( inv.Type )
-								{
-									case InvoiceType.Standard:
-										inv.AmountPaid += payment.Amount;
-										inv.Payments.Add( payment );
-										responseMessage = "another partial payment received, still not fully paid";
-										break;
-									case InvoiceType.Commercial:
-										inv.AmountPaid += payment.Amount;
-										inv.TaxAmount += payment.Amount * 0.14m;
-										inv.Payments.Add( payment );
-										responseMessage = "another partial payment received, still not fully paid";
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-							}
-						}
-					}
-					else
-					{
-						if ( payment.Amount > inv.Amount )
-						{
-							responseMessage = "the payment is greater than the invoice amount";
-						}
-						else if ( inv.Amount == payment.Amount )
-						{
-							switch ( inv.Type )
-							{
-								case InvoiceType.Standard:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now fully paid";
-									break;
-								case InvoiceType.Commercial:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now fully paid";
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
-						}
-						else
-						{
-							switch ( inv.Type )
-							{
-								case InvoiceType.Standard:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now partially paid";
-									break;
-								case InvoiceType.Commercial:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now partially paid";
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
-						}
-					}
-				}
-			}
-			
-			inv.Save();
+                if (invoice == null)
+                {
+                    string invoiceNullErrorMessage = "There is no invoice matching this payment"; // create variable instead of repeatitive string message
 
-			return responseMessage;
-		}
-	}
+                    LogError(invoiceNullErrorMessage);
+
+                    throw new InvalidOperationException(invoiceNullErrorMessage);
+                }
+
+                if (invoice.Amount == 0)
+                {
+                    if (invoice.Payments == null || !invoice.Payments.Any())
+                    {
+                        responseMessage = "No payment needed";
+                    }
+                    else
+                    {
+                        string invalidInvoiceErrorMessage = "The invoice is in an invalid state: it has an amount of 0 and it has payments."; // create variable instead of repeatitive string message
+
+                        LogError(invalidInvoiceErrorMessage);
+
+                        throw new InvalidOperationException(invalidInvoiceErrorMessage);
+                    }
+                }
+                else
+                {
+                    if (invoice.Payments != null && invoice.Payments.Any())
+                    {
+                        HandleExistingPayments(invoice, payment, ref responseMessage);
+                    }
+                    else
+                    {
+                        HandleNewPayment(invoice, payment, ref responseMessage);
+                    }
+                }
+
+                
+                await SaveInvoice(invoice, cancellationToken);
+
+                return responseMessage;
             }
             catch (Exception ex)
             {
@@ -157,25 +77,140 @@ namespace RefactorThis.Domain
 
                 throw ex;
             }
+        }
+
+        public async Task<Invoice> GetInvoice(string reference, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var invoice = await _invoiceRepository.GetInvoice(reference, cancellationToken);
+
+                return invoice;
+            }
             catch (Exception ex)
             {
                 LogError($"Method:[GetInvoice] An error occurred while fetching invoice: {ex.Message}, stackTrace: {ex.StackTrace}");
 
                 return null;
             }
+        }
+
+        public async Task SaveInvoice(Invoice invoice, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _invoiceRepository.SaveInvoice(invoice, cancellationToken);
+            }
             catch (Exception ex)
             {
                 LogError($"Method:[SaveInvoice] An error occurred while saving invoice: {ex.Message}, stackTrace: {ex.StackTrace}");
                 throw ex;
             }
+        }
+
+        //Method to add invoice payments
+        public void CalculateTax(Invoice invoice, Payment payment)
+        {
+            if (invoice.Type == InvoiceType.Commercial)
+            {
+                invoice.TaxAmount += payment.Amount * 0.14m;
+            }
+        }
+
+        private void HandleExistingPayments(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            try
+            {
+                var invoicePaymentSum = invoice.Payments.Sum(x => x.Amount);
+
+                if (invoicePaymentSum != 0 && invoice.Amount == invoicePaymentSum)
+                {
+                    responseMessage = "Invoice was already fully paid";
+                }
+                else if (invoicePaymentSum != 0 && payment.Amount > (invoice.Amount - invoice.AmountPaid))
+                {
+                    responseMessage = "The payment is greater than the partial amount remaining";
+                }
+                else
+                {
+                    if ((invoice.Amount - invoice.AmountPaid) == payment.Amount)
+                    {
+                        ProcessFinalPartialPayment(invoice, payment, ref responseMessage);
+                    }
+                    else
+                    {
+                        ProcessAnotherPartialPayment(invoice, payment, ref responseMessage);
+                    }
+                }
+            }
             catch (Exception ex)
             {
                 LogError($"Method:[HandleExistingPayments] An error occurred while handling existing payments: {ex.Message}, stackTrace: {ex.StackTrace}");
+            }
+        }
+
+        private void HandleNewPayment(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            try
+            {
+                if (payment.Amount > invoice.Amount)
+                {
+                    responseMessage = "The payment is greater than the invoice amount";
+                }
+                else if (invoice.Amount == payment.Amount)
+                {
+                    ProcessFullPayment(invoice, payment, ref responseMessage);
+                }
+                else
+                {
+                    ProcessPartialPayment(invoice, payment, ref responseMessage);
+                }
             }
             catch (Exception ex)
             {
                 LogError($"Method:[HandleNewPayment] An error occurred while handling new payment: {ex.Message}, stackTrace: {ex.StackTrace}");
             }
+        }
+
+        //Final partial payment processor
+        private void ProcessFinalPartialPayment(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            invoice.AmountPaid += payment.Amount;
+            AddPayment(invoice, payment);
+            responseMessage = "Final partial payment received, invoice is now fully paid";
+        }
+
+        //Another partial payment processor
+        private void ProcessAnotherPartialPayment(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            AddPayment(invoice, payment);
+            responseMessage = "Another partial payment received, still not fully paid";
+        }
+
+        //Full payment processor
+        private void ProcessFullPayment(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            invoice.AmountPaid = payment.Amount;
+            CalculateTax(invoice, payment);
+            AddPayment(invoice, payment);
+            responseMessage = "Invoice is now fully paid";
+        }
+
+        //Partial payment processor
+        private void ProcessPartialPayment(Invoice invoice, Payment payment, ref string responseMessage)
+        {
+            invoice.AmountPaid = payment.Amount;
+            CalculateTax(invoice, payment);
+            AddPayment(invoice, payment);
+            responseMessage = "Invoice is now partially paid";
+        }
+
+        //Method to add invoice payments
+        private void AddPayment(Invoice invoice, Payment payment)
+        {
+            invoice.Payments.Add(payment);
+        }
+
         //Method for adding error logs.
         //For now it's isolated for payment processing but it's recommended to make it global and re-usable
         private void LogError(string errorMessage)
