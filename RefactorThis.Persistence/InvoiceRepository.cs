8d31d539b1bee7b3b7f36cb905842d05d1987644
badicloud2011 <badicloud2011@gmail.com>
2024-05-10 01:17:22 +0800

    - Implement IInvoiceRepository interface to provide signature of events to interact database.
    - Uses async await for scalability, responsive and avoid blocking I/O operations
    - Implement asynchronous operation for scalable DB transaction and,
    - Use cancellation token to release allocated resources associated with asynchronous operations when they are canceled

--- RefactorThis.Persistence/InvoiceRepository.cs
+++ RefactorThis.Persistence/InvoiceRepository.cs
@@ -1,21 +1,37 @@
-namespace RefactorThis.Persistence {
-	public class InvoiceRepository
+using RefactorThis.Persistence.Interface;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RefactorThis.Persistence
+{
+    //Register this in Dependency Injection Service
+	public class InvoiceRepository : IInvoiceRepository
 	{
 		private Invoice _invoice;
 
-		public Invoice GetInvoice( string reference )
-		{
-			return _invoice;
+        public Task<Invoice> GetInvoice(string reference, CancellationToken cancellationToken = default)
+        {
+            // Simulate retrieving the invoice asynchronously from a database
+            // For example:
+            // return _databaseContext.Invoices.FindAsync(reference, cancellationToken);
+            return Task.FromResult(_invoice);
 		}
 
-		public void SaveInvoice( Invoice invoice )
-		{
-			//saves the invoice to the database
+        public Task SaveInvoice(Invoice invoice, CancellationToken cancellationToken = default)
+        {
+            // Simulate saving the invoice to the database asynchronously
+            // For example:
+            // return _databaseContext.Invoices.AddAsync(invoice, cancellationToken);
+            return Task.CompletedTask;
 		}
 
-		public void Add( Invoice invoice )
-		{
-			_invoice = invoice;
-		}
-	}
+        public Task Add(Invoice invoice)
+        {
+            // Simulate adding the invoice to the repository
+            // For example:
+            // _databaseContext.Invoices.Add(invoice);
+            _invoice = invoice;
+            return Task.CompletedTask;
+        }
+    }
 }
