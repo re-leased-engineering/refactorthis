e3049c56e3c7f785633819aabf735341a04a7649
badicloud2011 <badicloud2011@gmail.com>
2024-05-10 01:18:25 +0800

    - Implement IInvoiceRepository interface that will perform the database transaction.
    - Specified to register repository class and interface in Dependecy Injection Service once consumed in actual process and logic
    - Added comment for DB simulation

---
+++ RefactorThis.Persistence/Interface/IInvoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace RefactorThis.Persistence.Interface
+{
+    public interface IInvoiceRepository
+    {
+        // Uses async/await to perform non-blocking I/O operations, ensuring responsiveness and scalability.
+        // Accepts a CancellationToken to support cancellation of the operation. Release allocated resources associated with asynchronous operations when they are canceled
+        // Why Tasks? Asynchronous Execution, Scalable, Simplifies asynchronous programming with mechanisms, Supports cancellation of asynchronous operations
+        Task<Invoice> GetInvoice(string reference, CancellationToken cancellationToken);
+        Task SaveInvoice(Invoice invoice, CancellationToken cancellationToken);
+        Task Add(Invoice invoice);
+    }
+}
