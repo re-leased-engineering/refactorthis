8bfeb488b062746a5ac9829d3a4d73851ec56451
badicloud2011 <badicloud2011@gmail.com>
2024-05-10 01:18:49 +0800

    - Extract process of saving invoice and move it invoice service
    - Separate the EnumTypes in separate file so it will not mixed up with other class definition and easy to locate

---
+++ RefactorThis.Persistence/Enums/SytemEnums.cs
@@ -0,0 +1,9 @@
+﻿namespace RefactorThis.Persistence.Enums
+{
+	//Better to separate enums so it will not be mixed up to another class difinitions
+    public enum InvoiceType
+	{
+		Standard,
+		Commercial
+	}
+}
