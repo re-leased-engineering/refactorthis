db996345ba2d9aab4c4270f3b937c072560c5679
NZSol <robert.a.nickel@gmail.com>
2025-09-25 15:58:37 +1200

    Add NUnit test adapter | Update CSProj fiiles

--- RefactorThis.Domain.Tests/InvoicePaymentProcessorTests.cs
+++ RefactorThis.Domain.Tests/InvoicePaymentProcessorTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using NUnit.Framework;
-using RefactorThis.Persistence;
+using RefactorThis.Persistence.Model;
 
 namespace RefactorThis.Domain.Tests
 {
