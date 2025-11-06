6bd69098857b7822d390568753fdfad29dbaaa9b
NickelTing <125737200+NickelTing@users.noreply.github.com>
2025-11-07 12:03:06 +1300

    refactor code and test
    refactor the code by creating Booleans for criteria, promote type checks to catch variable type errors earlier, avoiding nesting by using if else statements as less as possible, and extract repetitive code in to methods for reusability and easier future improvement. Also made some changes to the test to accommodate the latest Nunit framework.

--- RefactorThis.Domain/InvoiceService.cs
+++ RefactorThis.Domain/InvoiceService.cs
@@ -8,142 +8,115 @@ namespace RefactorThis.Domain
 	{
 		private readonly InvoiceRepository _invoiceRepository;
 
-		public InvoiceService( InvoiceRepository invoiceRepository )
+		public InvoiceService(InvoiceRepository invoiceRepository)
 		{
 			_invoiceRepository = invoiceRepository;
 		}
 
-		public string ProcessPayment( Payment payment )
+		public string ProcessPayment(Payment payment)
 		{
-			var inv = _invoiceRepository.GetInvoice( payment.Reference );
+			Invoice inv = _invoiceRepository.GetInvoice(payment.Reference);  // change var to Invoice to enable type checking
+			string responseMessage = string.Empty; // change var to string 
 
-			var responseMessage = string.Empty;
-
-			if ( inv == null )
+			if (inv == null) // return error message if no invoice found
 			{
-				throw new InvalidOperationException( "There is no invoice matching this payment" );
+				throw new InvalidOperationException("There is no invoice matching this payment");
 			}
-			else
+
+			// write the conditions as a boolean
+			bool checkAmountZero = inv.Amount == 0;
+			bool checkInvoiceHasNoPayment = inv.Payments == null || !inv.Payments.Any();
+			bool isInvoicePaid = checkAmountZero && checkInvoiceHasNoPayment;
+
+			if (isInvoicePaid)
 			{
-				if ( inv.Amount == 0 )
+				if (checkInvoiceHasNoPayment) // if invoice amount is 0 and has no payment, then no payment needed
 				{
-					if ( inv.Payments == null || !inv.Payments.Any( ) )
+					responseMessage = "no payment needed";
+				}
+				else // throw an exception if ivalid invoice (0 amount but have payments, or non-zero amount but no payments)
+				{
+					throw new InvalidOperationException("The invoice is in an invalid state, it has an amount of 0 and it has payments.");
+				}
+			}
+			else  // if not paid, proceed to payment process
+			{
+				// write criteria and variables for payments.
+				bool checkInvoiceFullyPaid = inv.Amount == inv.Payments.Sum(x => x.Amount);
+				bool checkInvoiceHasPayment = inv.Payments.Sum(x => x.Amount) != 0;
+				decimal remainingInvoiceAmount = inv.Amount - inv.AmountPaid;
+				bool checkPaymentGreaterThanInvoiceAmount = payment.Amount > remainingInvoiceAmount;
+				bool checkPartialPaymentFull = (inv.Amount - inv.AmountPaid) == payment.Amount;
+				decimal paymentSubtractInvoiceAmount = payment.Amount - inv.Amount;
+
+				if (!checkInvoiceHasNoPayment) // if invoice already has payments
+				{
+					if (checkInvoiceHasPayment && checkInvoiceFullyPaid)
 					{
-						responseMessage = "no payment needed";
+						responseMessage = "invoice was already fully paid";
+					}
+					else if (checkInvoiceHasPayment && checkPaymentGreaterThanInvoiceAmount)
+					{
+						responseMessage = "the payment is greater than the partial amount remaining";
 					}
 					else
 					{
-						throw new InvalidOperationException( "The invoice is in an invalid state, it has an amount of 0 and it has payments." );
+                        calculateAmountBasedOnInvType(inv, payment); // extract the switch case to a method to improve readability and easier future improvement 
+
+						// at last, set the response message based on full or partial payment
+						if (checkPartialPaymentFull)
+						{
+							responseMessage = "final partial payment received, invoice is now fully paid";
+						}
+						else
+						{
+							responseMessage = "another partial payment received, still not fully paid";
+						}
 					}
 				}
-				else
+				else // otherwise, invoice has no existing payment
 				{
-					if ( inv.Payments != null && inv.Payments.Any( ) )
+					if (paymentSubtractInvoiceAmount > 0)
 					{
-						if ( inv.Payments.Sum( x => x.Amount ) != 0 && inv.Amount == inv.Payments.Sum( x => x.Amount ) )
-						{
-							responseMessage = "invoice was already fully paid";
-						}
-						else if ( inv.Payments.Sum( x => x.Amount ) != 0 && payment.Amount > ( inv.Amount - inv.AmountPaid ) )
-						{
-							responseMessage = "the payment is greater than the partial amount remaining";
-						}
-						else
-						{
-							if ( ( inv.Amount - inv.AmountPaid ) == payment.Amount )
-							{
-								switch ( inv.Type )
-								{
-									case InvoiceType.Standard:
-										inv.AmountPaid += payment.Amount;
-										inv.Payments.Add( payment );
-										responseMessage = "final partial payment received, invoice is now fully paid";
-										break;
-									case InvoiceType.Commercial:
-										inv.AmountPaid += payment.Amount;
-										inv.TaxAmount += payment.Amount * 0.14m;
-										inv.Payments.Add( payment );
-										responseMessage = "final partial payment received, invoice is now fully paid";
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-								
-							}
-							else
-							{
-								switch ( inv.Type )
-								{
-									case InvoiceType.Standard:
-										inv.AmountPaid += payment.Amount;
-										inv.Payments.Add( payment );
-										responseMessage = "another partial payment received, still not fully paid";
-										break;
-									case InvoiceType.Commercial:
-										inv.AmountPaid += payment.Amount;
-										inv.TaxAmount += payment.Amount * 0.14m;
-										inv.Payments.Add( payment );
-										responseMessage = "another partial payment received, still not fully paid";
-										break;
-									default:
-										throw new ArgumentOutOfRangeException( );
-								}
-							}
-						}
+						responseMessage = "the payment is greater than the invoice amount";
 					}
 					else
 					{
-						if ( payment.Amount > inv.Amount )
+						calculateAmountBasedOnInvType(inv, payment);
+
+						// at last, set the response message based on full or partial payment
+						if (paymentSubtractInvoiceAmount == 0)
 						{
-							responseMessage = "the payment is greater than the invoice amount";
-						}
-						else if ( inv.Amount == payment.Amount )
-						{
-							switch ( inv.Type )
-							{
-								case InvoiceType.Standard:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now fully paid";
-									break;
-								case InvoiceType.Commercial:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now fully paid";
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
+							responseMessage = "invoice is now fully paid";
 						}
 						else
 						{
-							switch ( inv.Type )
-							{
-								case InvoiceType.Standard:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now partially paid";
-									break;
-								case InvoiceType.Commercial:
-									inv.AmountPaid = payment.Amount;
-									inv.TaxAmount = payment.Amount * 0.14m;
-									inv.Payments.Add( payment );
-									responseMessage = "invoice is now partially paid";
-									break;
-								default:
-									throw new ArgumentOutOfRangeException( );
-							}
+							responseMessage = "invoice is now partially paid";
 						}
 					}
 				}
 			}
-			
+
 			inv.Save();
-
 			return responseMessage;
 		}
+
+
+		public void calculateAmountBasedOnInvType(Invoice inv, Payment payment)
+		{
+			switch (inv.Type) // do switch case first to remove further repitition
+			{
+				case InvoiceType.Standard:
+					break; // do nothing (I think here should not inclide tax amount? there should is a bug to include tax to standard invoice in the old code)
+				case InvoiceType.Commercial:
+					inv.TaxAmount += payment.Amount * 0.14m; // include tax for commercial invoice
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+            }
+            // move below code out of switch statement to avoid repetition
+            inv.AmountPaid += payment.Amount;
+            inv.Payments.Add(payment);
+        }
 	}
 }
